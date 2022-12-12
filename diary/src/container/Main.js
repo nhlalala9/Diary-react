fb9668d5feca305f521dc9c977f7a31c37bccee6
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2022-12-12 15:46:33 +0200

    button

--- diary/src/container/Main.js
+++ diary/src/container/Main.js
@@ -2,39 +2,57 @@ import React, { Component } from "react";
 // import PropTypes from 'prop-types'
 import { connect } from "react-redux";
 import DiaryForm from "../components/DiaryForm";
-import {addItem} from "../redux/actions";
+import { addItem } from "../redux/actions";
 import DiaryItem from "../components/DiaryItem";
 
 export class Main extends Component {
-    //   static propTypes = {
-    //     prop: PropTypes
-    //   }
+    constructor(){
+        super();
+        this.state = {
+           show: false
+        };
+    }
 
     render() {
 
         const { addItem, diaryItems } = this.props
         return (
-            <div className="grid-container">
-                <div className="diary-app" style={{paddingTop:"20px"}}>
-                    <h1>Diary</h1>
-                    <DiaryForm addItem={(item) => addItem(item)} />
+            <div>
+                <div className="grid-container">
+                    <div className="diary-app" style={{ paddingTop: "20px" }}>
+                        <h1>Diary</h1>
+                        <DiaryForm addItem={(item) => addItem(item)} />
 
-                </div>
-                <div className="diary-app">
-                    {diaryItems.length > 0 ? (diaryItems.map((item) => {
-                        return(
-                            <DiaryItem item={item}/>
+                    </div>
+                    <div className="diary-app">
+                        {diaryItems.length > 0 ? (diaryItems.map((item) => {
+                            return (
+                                <DiaryItem item={item} />
+                            )
+                        })
+
                         )
-                    })
+                            : (
+                                <h1>No Items</h1>
+                            )}
 
-                    )
-                        : (
-                            <h1>No Items</h1>
-                        )}
-
+                    </div>
+                    <Modal
+                        size="lg"
+                        show={show}
+                        onHide={() => setLgShow(false)}
+                        aria-labelledby="example-modal-sizes-title-lg"
+                    >
+                        <Modal.Header closeButton>
+                            <Modal.Title id="example-modal-sizes-title-lg">
+                                Large Modal
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>...</Modal.Body>
+                    </Modal>
                 </div>
             </div>
-        )
+        );
     }
 }
 
