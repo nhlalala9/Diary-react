cb14967e235d9eaceab6213eb351ab3e9aa17c8d
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2022-12-12 15:20:11 +0200

    diary

--- diary/src/App.js
+++ diary/src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
-import {store, persistor} from './redux/store'
+// import { persistor} from './redux/store'
+import persistor from "./redux/store"
+import store from "./redux/store"
 import {Provider} from "react-redux"
 import {PersistGate} from 'redux-persist/integration/react'
 import Main from "./container/Main"
--- diary/src/container/Main.js
+++ diary/src/container/Main.js
@@ -1,32 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 // import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
+import diaryForm from "../components/diaryForm";
+import {addItem} from "../redux/actions";
+import diaryItem from "../components/diaryItem";
 
 export class Main extends Component {
-//   static propTypes = {
-//     prop: PropTypes
-//   }
+    //   static propTypes = {
+    //     prop: PropTypes
+    //   }
 
-  render() {
+    render() {
 
-   const {addItem, diaryItems} =this.props
-    return (
-      <div className="grid-container">
-        <div className="diary-app">
-            <h1>Diary</h1>
-            <diaryForm addItem={(item) =>addItem(item)}/>
+        const { addItem, diaryItems } = this.props
+        return (
+            <div className="grid-container">
+                <div className="diary-app">
+                    <h1>Diary</h1>
+                    <diaryForm addItem={(item) => addItem(item)} />
 
-        </div>
-      </div>
-    )
-  }
+                </div>
+                <div className="diary-app">
+                    {diaryItems.length > 0 ? (diaryItems.map((item) => {
+                        <diaryItem/>
+                    })
+
+                    )
+                        : (
+                            <h1>No Items</h1>
+                        )}
+
+                </div>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state) => ({
-  diaryItems: state.diaryItems
+    diaryItems: state.diaryItems
 })
 
-const mapDispatchToProps = (dispatch) =>({
+const mapDispatchToProps = (dispatch) => ({
     addItem: (item) => dispatch(addItem(item))
 })
 
